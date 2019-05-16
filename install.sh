369e10e97cb58a2f75c1f6f40c0c1a12dea8fab9
Paul <pb66@users.noreply.github.com>
2019-05-16 10:57:41 +0100

    Update install.sh
    No longer need to create log folder as service unit does that.
    Symlink service unit rather than cp so that changes can easily be made to dev further.
    ExecStart line is now corrected in service unit so doesn't require editing here.
    Create conf folder in generic (non-emonpi specific) location and add symlink to data/emonhub.conf to catch all cases.

--- install.sh
+++ install.sh
@@ -58,11 +58,6 @@ service=emonhub
 emonhub_src_path=$usrdir/emonhub/src
 emonhub_conf_path=$usrdir/data
 
-emonhub_logfile_path=/var/log/emonhub
-if [ ! -d /var/log/emonhub ]; then
-    sudo mkdir $emonhub_logfile_path
-    sudo chown emonhub $emonhub_logfile_path
-fi
 
 if [ -f /lib/systemd/system/$service.service ]; then
     echo "- reinstalling $service.service"
@@ -73,9 +68,11 @@ else
     echo "- installing $service.service"
 fi
 
-sudo cp $usrdir/emonhub/service/$service.service /lib/systemd/system
-# Set ExecStart path to point to installed script and config location
-sudo sed -i "s~ExecStart=.*~ExecStart=$emonhub_src_path/emonhub.py --config-file=$emonhub_conf_path/emonhub.conf --logfile=$emonhub_logfile_path/emonhub.log~" /lib/systemd/system/$service.service
+sudo ln -sf $usrdir/emonhub/service/$service.service /lib/systemd/system
+
+sudo mkdir -p /etc/emonhub
+sudo ln -s $emonhub_conf_path/emonhub.conf /etc/emonhub
+
 sudo systemctl enable $service.service
 sudo systemctl restart $service.service
 
